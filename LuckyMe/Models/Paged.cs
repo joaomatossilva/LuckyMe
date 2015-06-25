646eab4e43f35cb827728b1c75003bc895b1b148
Joao Matos Silva <kappy@acydburne.com.pt>
2015-06-25 22:43:28 +0100

    Added paging on listing

---
+++ LuckyMe/Models/Paged.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Ajax.Utilities;
+
+namespace LuckyMe.Models
+{
+    public class Paged<T> : IPaged
+    {
+        public Paged()
+        {
+            ItemsPerPage = 10;
+        } 
+
+        private const int MaxShownPages = 7;
+
+        public IEnumerable<T> Items { get; set; }
+
+        public int ItemsTotalCount { get; set; }
+        public int ItemsPerPage { get; set; }
+        public int CurrentPage { get; set; }
+
+        public int TotalPages
+        {
+            get { return (int) Math.Ceiling(ItemsTotalCount/(decimal) ItemsPerPage); }
+        }
+
+        public int StartPage
+        {
+            get
+            {
+                int minStartPage = 1;
+                var totalPages = TotalPages;
+                if (totalPages <= MaxShownPages)
+                {
+                    return minStartPage;
+                }
+                minStartPage = CurrentPage - MaxShownPages/2;
+                return minStartPage <= 0 ? 1 : minStartPage;
+            }
+        }
+
+        public int EndPage
+        {
+            get
+            {
+                var maxEndPge = StartPage + MaxShownPages;
+                return maxEndPge > TotalPages ? TotalPages : maxEndPge - 1;
+            }
+        }
+    }
+
+    public interface IPaged
+    {
+        int CurrentPage { get; }
+        int StartPage { get; }
+        int EndPage { get; }
+    }
+}
